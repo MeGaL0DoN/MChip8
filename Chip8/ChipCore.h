83f0490a550de219928277c1995543df457a11dc
MeGaLoDoN228 <62940883+MeGaLoDoN228@users.noreply.github.com>
2024-03-13 21:51:49 -0700

    Add project files.

---
+++ Chip8/ChipCore.h
@@ -0,0 +1,331 @@
+#include <bitset>
+#include <cstddef>
+#include <iostream>
+#include <fstream>
+#include <random>
+#include "MiniAudio/miniaudio.h"
+
+class ChipCore
+{
+public:
+	static constexpr int SCRWidth = 64;
+	static constexpr int SCRHeight = 32;
+
+	int CPUfrequency { 500 };
+	bool enableSound { true };
+
+	ChipCore()
+	{
+		initialize();
+		ma_engine_init(NULL, &soundEngine);
+	}
+
+	inline bool getPixel(uint8_t x, uint8_t y)
+	{
+		return screenBuffer[SCRWidth * y + x];
+	}
+	inline void setKey(uint8_t key, bool isPressed)
+	{
+		keys[key] = isPressed;
+		if (inputReg != nullptr && !isPressed)
+		{
+			*inputReg = key;
+			inputReg = nullptr;
+		}
+	}
+
+	void loadROM(std::string_view path)
+	{
+		initialize();
+
+		std::ifstream ifs(path.data(), std::ios::binary | std::ios::ate);
+		std::ifstream::pos_type pos = ifs.tellg();
+
+		ifs.seekg(0, std::ios::beg);
+		ifs.read(reinterpret_cast<char*>(&RAM[0x200]), pos);
+		ifs.close();
+	}
+
+	void updateTimers()
+	{
+		if (delay_timer > 0) delay_timer--;
+		if (sound_timer > 0)
+		{
+			sound_timer--;
+			if (enableSound) beep();
+		}
+	}
+
+	void emulateCycle()
+	{
+		if (inputReg != nullptr) return;
+
+		const uint16_t opcode = (RAM[pc] << 8) | RAM[pc + 1];
+		bool incrementCounter { true };
+
+		uint8_t& regX = V[(opcode & 0x0F00) >> 8];
+		const uint8_t regY = V[(opcode & 0x00F0) >> 4];
+
+		const uint8_t doubleNibble = opcode & 0x00FF;
+		const uint16_t memoryAddr = opcode & 0x0FFF;
+
+		switch (opcode & 0xF000)
+		{
+		case 0x0000:
+		{
+			switch (opcode & 0x000F)
+			{
+			case 0x0000: 
+				clearScreen();
+				break;
+			case 0x000E: 
+				pc = stack[--sp];
+				break;
+			}
+			break;
+		}
+		case 0x1000: 
+			pc = memoryAddr;
+			incrementCounter = false;
+			break;  
+		case 0x2000:
+			stack[sp++] = pc;
+			pc = memoryAddr;
+			incrementCounter = false;
+			break;
+		case 0x3000:
+			if (regX == doubleNibble) skipNextInstr();
+			break;
+		case 0x4000:
+			if (regX != doubleNibble) skipNextInstr();
+			break;
+		case 0x5000:
+			if (regX == regY) skipNextInstr();
+			break;
+		case 0x6000:
+			regX = doubleNibble;
+			break;
+		case 0x7000: 
+			regX += doubleNibble;
+			break;
+		case 0x8000:
+			switch (opcode & 0x000F)
+			{
+			case 0x0000:
+				regX = regY;
+				break;
+			case 0x0001:
+				regX |= regY;
+				break;
+			case 0x0002:
+				regX &= regY;
+				break;
+			case 0x0003:
+				regX ^= regY;
+				break;
+			case 0x0004:
+			{
+				int result = regX + regY;
+				regX = result;
+				V[0xF] = result > 255;
+				break;
+			}
+			case 0x0005:
+			{
+				int result = regX - regY;
+				regX = result;
+				V[0xF] = result >= 0;
+				break;
+			}
+			case 0x0006: 
+			{
+			//	regX = regY;
+				uint8_t lsb = regX & 1;
+				regX >>= 1;
+				V[0xF] = lsb;
+				break;
+			}
+			case 0x0007: 
+				regX = regY - regX;
+				V[0xF] = regY >= regX;
+				break;
+			case 0x000E: 
+			{
+				//regX = regY;
+				uint8_t msb = (regX & 0x80) >> 7;
+				regX <<= 1;
+				V[0xF] = msb;
+				break;
+			}
+			}
+			break;
+		case 0x9000:
+			if (regX != regY) skipNextInstr();
+			break;
+		case 0xA000:
+			I = memoryAddr;
+			break;
+		case 0xB000:
+			pc = V[0] + memoryAddr;
+			incrementCounter = false;
+			break;
+		case 0xC000:
+			regX = rngDistr(rngEng) & doubleNibble;
+			break;
+		case 0xD000: 
+			drawSprite(regX % SCRWidth, regY % SCRHeight, opcode & 0x000F);
+			break;
+		case 0xE000:
+			switch (opcode & 0x000F)
+			{
+			case 0x000E:
+				if (keys[regX]) skipNextInstr();
+				break;
+			case 0x0001:
+				if (!keys[regX]) skipNextInstr();
+				break;
+			}
+			break;
+		case 0xF000:
+			switch (opcode & 0x00FF)
+			{
+			case 0x0007:
+				regX = delay_timer;
+				break;
+			case 0x000A: 
+				inputReg = &regX;
+				break;
+			case 0x001E:
+				I += regX;
+				break;
+			case 0x0015:
+				delay_timer = regX;
+				break;
+			case 0x0018:
+				sound_timer = regX;
+				break;
+			case 0x0029:
+				I = regX * 0x5;
+				break;
+			case 0x0033:
+				RAM[I] = regX / 100;
+				RAM[I + 1] = (regX / 10) % 10;
+				RAM[I + 2] = (regX % 100) % 10;
+				break;
+			case 0x0055:
+				for (int i = 0; i <= ((opcode & 0x0F00) >> 8); i++)
+					RAM[I + i] = V[i];
+				break;
+			case 0x0065:
+				for (int i = 0; i <= ((opcode & 0x0F00) >> 8); i++)
+					V[i] = RAM[I + i];
+				break;
+			}
+			break;
+		}
+
+		if (incrementCounter)
+			pc += 2;
+	}
+
+private:
+	std::bitset<SCRWidth * SCRHeight> screenBuffer{};
+	uint8_t RAM[4096];
+
+	uint8_t V[16];
+	uint16_t I;
+	uint16_t pc;
+
+	uint8_t delay_timer;
+	uint8_t sound_timer;
+
+	uint16_t stack[16];
+	uint16_t sp;
+
+	std::bitset<16> keys{};
+	uint8_t* inputReg;
+
+	std::default_random_engine rngEng { std::random_device{}() };
+	std::uniform_int_distribution<> rngDistr { 0, 255 };
+
+	static constexpr uint8_t fontset[80] =
+	{
+		0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+		0x20, 0x60, 0x20, 0x20, 0x70, // 1
+		0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+		0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+		0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+		0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+		0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+		0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+		0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+		0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+		0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+		0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+		0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+		0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+		0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+		0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+	};
+
+	ma_engine soundEngine;
+	inline void beep()
+	{
+		ma_engine_play_sound(&soundEngine, "data/beep.wav", NULL);
+	}
+
+	inline void clearScreen()
+	{
+		screenBuffer.reset();
+	}
+	inline void setPixel(uint8_t x, uint8_t y, bool val)
+	{
+		screenBuffer[SCRWidth * y + x] = val;
+	}
+
+	inline void drawSprite(uint8_t Xpos, uint8_t Ypos, uint8_t height)
+	{
+		constexpr uint8_t width = 8;
+		V[0xF] = 0; 
+
+		for (int i = 0; i < height; i++)
+		{
+			uint8_t spriteRow = RAM[I + i];
+			uint8_t screenY = i + Ypos;
+			if (screenY >= SCRHeight) return;
+
+			for (int j = 0; j < width; j++)
+			{
+				bool spritePixel = spriteRow & (0x80 >> j);
+				if (spritePixel)
+				{
+					uint8_t screenX = j + Xpos;
+					if (screenX >= SCRWidth) return;
+
+					bool screenPixel = getPixel(screenX, screenY);
+					if (screenPixel) V[0xF] = 1;
+					setPixel(screenX, screenY, !screenPixel);
+				}
+			}
+		}
+	}
+
+	constexpr void skipNextInstr() { pc += 2; }
+
+	void initialize()
+	{
+		pc = 0x200;  
+		I = 0;      
+		sp = 0;
+		delay_timer = 0;
+		sound_timer = 0;
+
+		std::memset(V, 0, sizeof(V));
+		std::memset(RAM, 0, sizeof(RAM));
+		std::memcpy(RAM, fontset, sizeof(fontset));
+
+		clearScreen();
+		keys.reset();
+		inputReg = nullptr;
+	}
+};
---
+++ Chip8/Main.cpp
@@ -0,0 +1,378 @@
+#define MINIAUDIO_IMPLEMENTATION
+
+#include "ImGui/imgui.h"
+#include "ImGui/imgui_impl_glfw.h"
+#include "ImGui/imgui_impl_opengl3.h"
+
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+#include "nfd/nfd.h"
+
+#include <array>
+#include <map>
+#include <sstream>
+#include <iostream>   
+#include <filesystem>
+
+#include "Shader.h"
+#include "ChipCore.h"
+
+ChipCore chipCore {};
+bool pause { false };
+bool pixelBorders { false };
+
+int menuBarHeight;
+GLFWwindow* window;
+
+Shader pixelShader;
+unsigned int VBO;
+
+float pixel_XGap, pixel_YGap;
+float widthUnit, heightUnit;
+int viewport_width, viewport_height;
+
+std::string currentPath { std::filesystem::current_path().string() };
+
+void draw() {
+    for (int x = 0; x < ChipCore::SCRWidth; x++)
+    {
+        for (int y = 0; y < ChipCore::SCRHeight; y++)
+        {
+            if (chipCore.getPixel(x, y))
+            {
+                pixelShader.setFloat2("offset", x * (widthUnit + pixel_XGap), y * (heightUnit + pixel_YGap));
+                glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+            }
+        }
+    }
+}
+
+std::array<float, 12> getVertices()
+{
+    pixel_XGap = pixelBorders ? 2.0f / viewport_width : 0; 
+    pixel_YGap = pixelBorders ? 2.0f / viewport_height : 0;
+
+    widthUnit = 2.0f / (ChipCore::SCRWidth) - pixel_XGap;
+    heightUnit = 2.0f / (ChipCore::SCRHeight) - pixel_YGap;
+
+    return {
+        -1.0f + widthUnit, 1.0f, 0.0f,  // top right
+        -1.0f + widthUnit, 1.0f - heightUnit, 0.0f,  // bottom right
+        -1.0f, 1.0f - heightUnit, 0.0f,  // bottom left
+        -1.0f, 1.0f, 0.0f   // top left 
+    };
+}
+
+void updateVertices()
+{
+    auto vertices = getVertices();
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices.data());
+}
+
+void setBuffers()
+{
+    unsigned int VAO, EBO;
+    constexpr unsigned int indices[] = {
+        0, 1, 3,
+        1, 2, 3
+    };
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    auto vertices = getVertices();
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices.data(), GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+std::string currentROMPAth {};
+void loadROM(std::string_view path)
+{
+    chipCore.loadROM(path);
+    pause = false;
+    currentROMPAth = path;
+}
+
+void renderImGUI()
+{
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+
+    if (ImGui::BeginMainMenuBar()) 
+    {
+        if (ImGui::BeginMenu("File")) 
+        {
+            if (ImGui::MenuItem("Load ROM")) 
+            {
+                nfdchar_t* outPath { nullptr };
+                nfdresult_t result = NFD_OpenDialog("ch8,bin", currentPath.c_str(), &outPath);
+
+                if (result == NFD_OKAY) {
+                    loadROM(outPath);
+                    free(outPath);
+                }
+            }
+            else if (ImGui::MenuItem("Reload ROM", "(Esc)"))
+                loadROM(currentROMPAth);
+
+            ImGui::EndMenu();
+        }
+        if (ImGui::BeginMenu("Settings", "Ctrl+Q"))
+        {
+            static bool showForegroundPicker { false };
+            static bool showBackgroundPicker { false };
+
+            ImGui::SliderInt("CPU Frequency", &chipCore.CPUfrequency, 60, 1200);
+            ImGui::Separator();
+            ImGui::Checkbox("Enable Sound", &chipCore.enableSound);
+            ImGui::SeparatorText("UI");
+
+            if (ImGui::Checkbox("Pixel Borders", &pixelBorders))
+                updateVertices();
+
+            static ImVec4 foregroundColor = ImVec4(1.0f, 1.0f, 1.0f, 1.0f);
+            static ImVec4 backgroundColor = ImVec4(0.0f, 0.0f, 0.0f, 1.0f);
+
+            ImGui::Separator();
+            ImGui::Spacing();
+            ImGui::Text("Foreground Color");
+            ImGui::SameLine();
+
+            if (ImGui::ArrowButton("foregroundPicker", ImGuiDir_Down))
+            {
+                showForegroundPicker = !showForegroundPicker;
+                showBackgroundPicker = false;
+            }
+
+            if (showForegroundPicker)
+            {
+                if (ImGui::ColorPicker3("Pick a Color", (float*)&foregroundColor))
+                    pixelShader.setFloat4("color", (float*)&foregroundColor);
+            }
+
+            ImGui::Separator();
+            ImGui::Spacing();
+            ImGui::Text("Background Color");
+            ImGui::SameLine();
+
+            if (ImGui::ArrowButton("backgroundPicker", ImGuiDir_Down))
+            {
+                showBackgroundPicker = !showBackgroundPicker;
+                showForegroundPicker = false;
+            }
+
+            if (showBackgroundPicker)
+            {
+                if (ImGui::ColorPicker3("Pick a Color", (float*)&backgroundColor))
+                    glClearColor(backgroundColor.x, backgroundColor.y, backgroundColor.z, 1.0f);
+            }
+
+            ImGui::SeparatorText("Misc.");
+            if (ImGui::Button("Reset to Default"))
+            {
+                foregroundColor = ImVec4(1.0f, 1.0f, 1.0f, 1.0f);
+                backgroundColor = ImVec4(0.0f, 0.0f, 0.0f, 1.0f);
+                pixelBorders = false;
+
+                pixelShader.setFloat4("color", (float*)&foregroundColor);
+                glClearColor(backgroundColor.x, backgroundColor.y, backgroundColor.z, 1.0f);
+                updateVertices();
+
+                chipCore.CPUfrequency = 500;
+                chipCore.enableSound = true;
+            }
+
+            ImGui::EndMenu();
+        }
+        if (pause)
+        {
+            ImGui::Separator();
+            ImGui::Text("Paused");
+        }
+        ImGui::EndMainMenuBar();
+    }
+
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+}
+
+void render()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    draw();
+    renderImGUI();
+    glfwSwapBuffers(window);
+}
+
+std::map<int, uint8_t> keyConfig{};
+void loadKeyConfig()
+{
+    std::ifstream configFile("data/keyConfig.ini");
+    std::string line;
+
+    while (std::getline(configFile, line)) {
+        std::stringstream ss(line);
+        char chipKey;
+        int bindValue;
+
+        ss >> chipKey;
+        ss.ignore(1);
+        ss >> bindValue;
+
+        keyConfig[bindValue] = std::stoi(std::string{ chipKey }, 0, 16);
+    }
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (action == 1)
+    {
+        if (key == GLFW_KEY_ESCAPE)
+        {
+            loadROM(currentROMPAth);
+            return;
+        }
+        if (key == GLFW_KEY_TAB)
+        {
+            pause = !pause;
+            return;
+        }
+    }
+
+    auto keyInd = keyConfig.find(scancode);
+
+    if (keyInd != keyConfig.end())
+        chipCore.setKey(keyInd->second, action);
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    viewport_width = width; viewport_height = height - menuBarHeight;
+    glViewport(0, 0, viewport_width, viewport_height);
+    updateVertices();
+    render();
+}
+
+void setWindowSize()
+{
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+    ImGui::BeginMainMenuBar();
+    menuBarHeight = static_cast<int>(ImGui::GetWindowSize().y);
+    ImGui::EndMainMenuBar();
+    ImGui::Render();
+
+    const GLFWvidmode* mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+    viewport_width = { static_cast<int>(mode->width * 0.65f) };
+    viewport_height = viewport_width / 2;
+
+    glfwSetWindowSize(window, viewport_width, viewport_height + menuBarHeight);
+    glfwSetWindowAspectRatio(window, viewport_width, viewport_height + menuBarHeight);
+    glViewport(0, 0, viewport_width, viewport_height);
+}
+
+bool setGLFW()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    window = glfwCreateWindow(1, 1, "Chip8", NULL, NULL);
+    if (!window)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return false;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetKeyCallback(window, key_callback);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
+void setImGUI()
+{
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); 
+    io.IniFilename = "data/imgui.ini";
+    io.Fonts->AddFontFromFileTTF("data/roboto.ttf", 17);
+    ImGui::StyleColorsDark();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330");
+}
+
+int main() {
+    if (!setGLFW()) return -1;
+    setImGUI();
+    setWindowSize();
+    setBuffers();
+
+    pixelShader = Shader("data/Shaders/vertexShader.glsl", "data/Shaders/fragmentShader.glsl");
+    pixelShader.use();
+
+    float whiteColor[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+    pixelShader.setFloat4("color", whiteColor);
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+    loadKeyConfig();
+    loadROM("ROMs/chipLogo.ch8");
+
+    double lastFrameTime = glfwGetTime();
+    double cpuFrequencyTimer{};
+    double renderTimer{};
+
+    while (!glfwWindowShouldClose(window)) 
+    {
+        double currentFrameTime = glfwGetTime();
+        double deltaTime = currentFrameTime - lastFrameTime;
+
+        cpuFrequencyTimer += deltaTime;
+        renderTimer += deltaTime;
+
+        if (cpuFrequencyTimer >= 1.0 / chipCore.CPUfrequency)
+        {
+            if (!pause) chipCore.emulateCycle();
+            cpuFrequencyTimer = 0;
+        }
+        if (renderTimer >= 1.0 / 60)
+        {
+            renderTimer = 0;
+            if (!pause) chipCore.updateTimers();
+
+            glfwPollEvents();
+            render();
+        }
+
+        lastFrameTime = currentFrameTime;
+    }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+    glfwTerminate();
+
+	return 0;
+}
